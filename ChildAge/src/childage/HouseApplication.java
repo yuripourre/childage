90f43d64e9b88b982558586632913bff4c9c5f4a
Yuri Pourre <yuriamayuri@gmail.com>
2014-01-24 19:50:26 -0200

    Drawing Grid

--- ChildAge/src/childage/HouseApplication.java
+++ ChildAge/src/childage/HouseApplication.java
@@ -6,12 +6,61 @@ import br.com.etyllica.core.event.GUIEvent;
 import br.com.etyllica.core.event.KeyEvent;
 import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.video.Graphic;
+import childage.tiles.Floor;
 
 
 public class HouseApplication extends Application{
 
+	private Floor[][] floor;
+
+	private final int floorWidth = 25;
+	private final int floorHeight = 18;	
+
 	public HouseApplication(int w, int h) {
 		super(w, h);
+	}
+
+	@Override
+	public void load() {
+
+		floor = new Floor[floorHeight][floorWidth];
+
+		for(int j=0; j<floorHeight;j++){
+			
+			for(int i=0; i<floorWidth;i++){
+			
+				floor[j][i] = new Floor(i*Floor.TILE_SIZE, j*Floor.TILE_SIZE);
+				
+			}
+			
+		}
+
+		loading = 100;
+	}
+
+	@Override
+	public void draw(Graphic g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0,0,w,h);
+		
+		drawTiles(g);
+		
+	}
+	
+	private void drawTiles(Graphic g){
+		
+		g.setColor(Color.BLACK);
+		
+		for(int j=0; j<floorHeight;j++){
+			
+			for(int i=0; i<floorWidth;i++){
+			
+				g.drawRect(floor[j][i]);
+				
+			}
+			
+		}
+		
 	}
 
 	@Override
@@ -24,20 +73,6 @@ public class HouseApplication extends Application{
 	public GUIEvent updateKeyboard(KeyEvent event) {
 		// TODO Auto-generated method stub
 		return null;
-	}
+	}		
 
-	@Override
-	public void load() {
-		// TODO Auto-generated method stub
-		loading = 100;
-	}
-
-	@Override
-	public void draw(Graphic g) {
-		g.setColor(Color.BLUE);
-		g.fillRect(0,0,w,h);
-		
-	}
-		
-	
 }
---
+++ ChildAge/src/childage/tiles/Floor.java
@@ -0,0 +1,13 @@
+package childage.tiles;
+
+import br.com.etyllica.layer.GeometricLayer;
+
+public class Floor extends GeometricLayer{
+
+	public static final int TILE_SIZE = 32;
+	
+	public Floor(int x, int y){
+		super(x,y,TILE_SIZE,TILE_SIZE);
+	}
+	
+}
